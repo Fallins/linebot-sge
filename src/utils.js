2ec69a7b95c680fc036d96c5f0a8dd885e567730
Ben <ben@vgt-hk.net>
2018-12-18 17:14:29 +0800

    commit

--- src/utils.js
+++ src/utils.js
@@ -6,29 +6,48 @@ const analyzedIncomingMsg = event => {
   console.log({ event })
   console.log(`============== EVENT OBJECT END ==============`)
   const {
-    source: { type, groupId }
+    source: { type, groupId },
+    message: { text }
   } = event
 
   console.log({ type, groupId })
   // Only accept group to use
   if (type === 'group') {
-    if (groupObj[groupId]) {
-      // add info into groupObj
-      return getProfile(event).then(profile => {
-        groupObj[groupId].push(profile)
-        console.log({ groupObj: JSON.stringify(groupObj) })
-        replyText(event, profile.displayName + ' is in.')
-      })
-    } else {
-      // initailized group info
-      groupObj[groupId] = []
-      console.log({ groupObj })
-      return replyText(event, '可以抽獎了，請要參加的人回應任意文字')
+    switch (text) {
+      case '開始抽獎':
+        // initailized group info
+        groupObj[groupId] = []
+        console.log({ groupObj })
+        return replyText(event, '可以抽獎了，請要參加的人回覆 "+"、"++"、"+1" ')
+      case '+':
+      case '++':
+      case '+1':
+        // add info into groupObj
+        return getProfile(event).then(profile => {
+          groupObj[groupId].push(profile)
+          console.log({ groupObj: JSON.stringify(groupObj) })
+          replyText(event, profile.displayName + ' 參加成功')
+        })
+      case '準備完成':
+        return replyText(
+          event,
+          `目前參加的人數有 ${groupObj[groupId].length} 人\n
+           請依照下列順序回覆 "抽" 開始交換禮物
+           ${randomArr(groupObj[groupId]).map(
+             (player, idx) => `${idx + 1}. ${player.displayName}\n`
+           )} 
+          `
+        )
+      default:
+        return replyText(event, '指令錯誤')
     }
   }
 
   console.log({ groupObj })
-  return replyText(event, 'This bot is only accept group to play')
+  return replyText(
+    event,
+    '只能在群組中使用哦！\n趕快邀請朋友一起來交換禮物吧！！！'
+  )
 }
 
 const replyText = (event, text) => {
@@ -44,6 +63,14 @@ const getProfile = event => {
     console.log(profile)
     return profile
   })
+}
+
+const randomArr = arr => {
+  //用Math.random()函式生成0~1之間的隨機數與0.5比較，返回-1或1
+  const randomSort = () => (Math.random() > 0.5 ? -1 : 1)
+
+  arr.sort(randomSort)
+  return arr
 }
 
 const getMembersId = event => {
