14dddd042cf9aaa23ffbb4a8163d5a01c008ea88
Ben <ben@vgt-hk.net>
2018-12-18 15:37:26 +0800

    add log to watch profile and member info

--- src/utils.js
+++ src/utils.js
@@ -4,6 +4,9 @@ const analyzedIncomingMsg = event => {
   console.log({ event })
   console.log(`============== EVENT OBJECT END ==============`)
 
+  console.log(getProfile(event))
+  console.log(getMember(event))
+
   replyText(event, "I've received your message")
 }
 
@@ -13,6 +16,14 @@ const replyText = (event, text) => {
     console.log('Send text message failed')
     console.log(e)
   })
+}
+
+const getProfile = event => {
+  return event.source.profile().then(profile => profile)
+}
+
+const getMember = event => {
+  return event.source.member().then(member => member)
 }
 
 module.exports = {
