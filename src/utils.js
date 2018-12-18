cfa9561b36d5ed82c8fbd66852e5b62f1c59636d
Ben <ben@vgt-hk.net>
2018-12-18 16:19:09 +0800

    add logic

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,32 @@
+const groupObj = {}
+
 const analyzedIncomingMsg = event => {
   console.log(`================ EVENT OBJECT ================`)
   console.log(JSON.stringify(event))
   console.log({ event })
   console.log(`============== EVENT OBJECT END ==============`)
+  const {
+    source: { type }
+  } = event
 
-  getProfile(event)
-  getMembersId(event)
+  // Only accept group to use
+  if (type !== 'group') {
+    if (groupObj[groupId]) {
+      // add info into groupObj
+      getProfile(event).then(profile => {
+        groupObj[groupId].push(profile)
+        console.log(groupObj)
+      })
+    } else {
+      // initailized group info
+      groupObj[groupId] = []
 
-  replyText(event, "I've received your message")
+      replyText(event, '可以抽獎了，請要參加的人回應任意文字')
+    }
+  }
+
+  replyText(event, 'This bot is only accept group to play')
+  replyText(event, 'test22222')
 }
 
 const replyText = (event, text) => {
@@ -19,7 +38,10 @@ const replyText = (event, text) => {
 }
 
 const getProfile = event => {
-  return event.source.profile().then(profile => console.log(profile))
+  return event.source.profile().then(profile => {
+    console.log(profile)
+    return profile
+  })
 }
 
 const getMembersId = event => {
